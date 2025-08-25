0e70f95e62434137b9696474394fd8e8d02f011a
GustavOliveira04 <gufreitas254@gmail.com>
2025-08-25 16:30:59 -0300

    Realizando alguns testes

---
+++ 6-testes/tax_test.go
@@ -0,0 +1,46 @@
+package tax
+
+import "testing"
+
+func TestCalculateTax(t *testing.T) {
+	amount := 500.0
+	expected := 5.0
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %.2f but got %.2f", expected, result)
+	}
+}
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expect float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+		{2000.0, 10.0},
+		{100.0, 5.0},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expect {
+			t.Errorf("Expected %.2f but got %.2f", item.expect, result)
+		}
+	}
+}
+
+func BenchmarkCalculateTax(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax(500.0)
+	}
+}
+
+func BenchmarkCalculateTax2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax2(500.0)
+	}
+}
